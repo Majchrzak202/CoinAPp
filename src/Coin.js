6d99086bb46d16e88cc5eea91707f22659b55e47
MarcinMaster <m.luba505@gmail.com>
2022-05-14 07:33:58 +0200

    Removing dummy data

--- src/Coin.js
+++ src/Coin.js
@@ -4,9 +4,9 @@ import React from "react";
 const Coin = ({name, price}) => {
     return (
         <div>
-        <h1>{name}</h1>
-        <h2>{price}</h2>
-        <p>Zajebioza</p>
+        <h1>{name} {price.toFixed(2)} </h1>
+        <h2> Price:{price.toFixed(2)} $</h2>
+        
         </div>
     )
 }
