40f1f7ae62b85edff19e0e1edc3983f8f87039ec
MarcinMaster <m.luba505@gmail.com>
2022-05-16 07:09:14 +0200

    Adding Framer Motion dynamic styles

--- src/Coin.js
+++ src/Coin.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Coin = ({ name, price, icon, id }) => {
   return (
-    <div className="listitem">
-      <img className="image" src={icon}  alt={id}></img>
+    <motion.div
+      className="listitem"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      whileHover={{scale: 1.15}}
+      layout
+    >
+      <motion.img
+        className="image"
+        src={icon}
+        alt={id}
+        whileHover={{ rotate: 360, transition: {duration:1}}}x
+      ></motion.img>
       <h1>{name} </h1>
       <p> Price:{price.toFixed(2)} $</p>
-    </div>
+    </motion.div>
   );
 };
 
