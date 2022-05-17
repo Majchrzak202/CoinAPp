fb610bc85785a6707a05d54123f01a4267594058
MarcinMaster <m.luba505@gmail.com>
2022-05-17 09:02:25 +0200

    Rendering header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ListOfCrypto from "./ListOfCrypto";
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const api = {
   base: "https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=INR",
@@ -32,24 +33,26 @@ function App() {
       });
   };
 
-  console.log(crypto);
-
   return (
     <div>
-      <div className='up'>
-      <button onClick={fetchCrypto}> FETCH COINS!</button>
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <div>
+        <Header />
       </div>
-      
+      <div className="up">
+        {/* <button onClick={fetchCrypto}> FETCH COINS!</button> */}
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
       {isLoading && <p>Loading....</p>}
       <div /* className='section' */>
-      <ListOfCrypto search={search} crypto={crypto} />
-      {error && <p>Something went wrong ({error})</p>}
+        <ListOfCrypto search={search} crypto={crypto} />
+        {error && <p>Something went wrong ({error})</p>}
       </div>
+     <Footer /> 
     </div>
   );
 }
