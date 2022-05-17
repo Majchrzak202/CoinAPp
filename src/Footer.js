bed0912909df1e74184bab549ef64d63c4d72f8e
MarcinMaster <m.luba505@gmail.com>
2022-05-17 09:00:10 +0200

    Adding a Footer, Header and a NavBar

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+    <footer className='footer'>
+        <p>This is a random Footer</p>
+        <p> Copyright 2022</p>
+    </footer>)
+}
+
+export default Footer; 
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./navigation";
+
+const Header = () => {
+
+
+  return (
+    <header className="header">
+      <Navigation  />
+    </header>
+  );
+};
+
+export default Header;
