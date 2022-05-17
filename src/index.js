e15ae7dca521e60d63640639b00bc85faa40e382
MarcinMaster <m.luba505@gmail.com>
2022-05-17 09:01:03 +0200

    adding Auth0 provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain='dev--unc4no4.us.auth0.com'
+  clientId='FKNN2irhroDiMEr1Enrx5SEW7bX3Uudn'
+  redirectUri={window.location.origin}>
+
+    
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 
