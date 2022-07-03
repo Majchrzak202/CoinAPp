8d4beeee81ea88f33e85bef1e18d51934dd24f0b
MarcinMaster <m.luba505@gmail.com>
2022-07-03 07:23:26 +0200

    Added a function to handle adding coins to whislist given from contect

--- src/Coin.js
+++ src/Coin.js
@@ -1,20 +1,42 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useWishlist } from "./store/WishlistContextProvider";
 
 const Coin = ({ name, price, icon, id }) => {
+  const { addToWishlist } = useWishlist();
+
+  const addToWishListHandler = () => {
+    addToWishlist({
+      name,
+      icon,
+      id,
+      price
+    })
+  }
+
   return (
     <motion.div
       className="listitem"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      whileHover={{scale: 1.15}}
+      whileHover={{ scale: 1.15 }}
       layout
     >
+      <motion.img
+        whileTap={{
+          backgroundColor: ["hsl(0, 100, 50)", "hsl(-120, 100, 50)"],
+        }}
+        className="whishlist"
+        alt="img"
+        src="https://cdn1.iconfinder.com/data/icons/multimedia-control/100/Multimedia_-23-1024.png"
+      />
+      <button onClick={addToWishListHandler}>+</button>
+
       <motion.img
         className="image"
         src={icon}
         alt={id}
-        whileHover={{ rotate: 360, transition: {duration:1}}}x
+        whileHover={{ rotate: 360, transition: { duration: 1 } }}
       ></motion.img>
       <h1>{name} </h1>
       <p> Price:{price.toFixed(2)} $</p>
